ef686325df491de5a3f0a766e46ad6ce731d4710
EmiKilic <emirhan_kilic@outlook.de>
2024-12-20 12:04:58 +0100

    show user in sheet with ngFor

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,19 +5,39 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialog } from '@angular/material/dialog';
 import { MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { User } from '../../models/user.class';
+import { CommonModule, NgFor } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-user',
-  imports: [MatIcon, MatButtonModule, MatTooltipModule, MatDialogModule, MatCardModule],
+  imports: [
+    MatIcon,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatCardModule,
+    NgFor,
+    CommonModule
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
-  
-  constructor(public dialogRef: MatDialog) {
-    
+  user= new User();
+  allUsers: (User & { id: string })[] = [];
+
+  constructor(public dialogRef: MatDialog, private firestore: Firestore) {}
+
+  ngOnInit(): void {
+    const usersCollection = collection(this.firestore, 'users');
+    collectionData(usersCollection, { idField: 'id' }).subscribe((changes) => {
+      console.log('Received changes from DB:', changes);
+      this.allUsers = changes as (User & { id: string })[];
+    });
   }
 
   openDialog() {
