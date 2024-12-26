bcd057723965ce83ef6c9cd761060be6837d1efe
EmiKilic <emirhan_kilic@outlook.de>
2024-12-26 14:25:21 +0100

    get user id from url

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardTitle,
+} from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
   imports: [MatCard, MatCardContent, MatCardTitle, MatCardHeader],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrl: './user-detail.component.scss',
 })
 export class UserDetailComponent {
+  userId = '';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( paraMap => {
+      this.userId = paraMap.get('id') ?? '';
+      console.log('Got Id', this.userId);
+      
+    })
+  }
 }
